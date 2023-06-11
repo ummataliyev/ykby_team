bf00b9332d9b48b330745e1774939ab4eb982699
ummataliyev <djohnummataliyev@gmail.com>
2023-06-11 23:21:14 +0500

    Pagination

--- rooms/views.py
+++ rooms/views.py
@@ -1,8 +1,24 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
 from rest_framework.filters import SearchFilter
+from rest_framework.pagination import PageNumberPagination
 
 from .models import Room
 from .serializers import RoomSerializer
+
+
+class CustomPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 10
+
+    def get_paginated_response(self, data):
+        return Response({
+            'page': self.page.number,
+            'count': self.page.paginator.count,
+            'page_size': self.get_page_size(self.request),
+            'results': data
+        })
 
 
 class RoomsViewSet(viewsets.ModelViewSet):
@@ -10,3 +26,4 @@ class RoomsViewSet(viewsets.ModelViewSet):
     serializer_class = RoomSerializer
     filter_backends = [SearchFilter]
     search_fields = ['name']
+    pagination_class = CustomPagination
