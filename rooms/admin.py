614c1dccd0de5f01e27d7d91673e6db0d8347400
ummataliyev <djohnummataliyev@gmail.com>
2023-06-11 19:13:25 +0500

    Models

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Room
+from .models import Booking
+
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ['name', 'room_type', 'seats']
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ['room', 'start_time', 'end_time']
--- rooms/models.py
+++ rooms/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Room(models.Model):
+    name = models.CharField(max_length=120)
+    room_type = models.CharField(max_length=120)
+    seats = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Booking(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    start_time = models.DateField(null=True)
+    end_time = models.DateField(null=True)
+
+    def __str__(self):
+        return f"Booking for room: {self.room}\n- Start time: {self.start_time}\n- End time: {self.end_time}" # noqa
